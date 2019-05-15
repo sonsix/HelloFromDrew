171f324c3a4d24f838b50989277e35118ab36add
Flint64 <twda96@gmail.com>
2019-05-15 09:38:57 -0600

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Drew.");
         System.out.println("Hello Drew, my name is Travis.");
+        System.out.println("What's up man?.");
     }
 }

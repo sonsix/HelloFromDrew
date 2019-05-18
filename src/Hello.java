7c9442595b58c32f95227d37a13f004c20039799
Detroitiron <mecham.shane@gmail.com>
2019-05-18 12:22:31 -0600

    Said Hello

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hello Drew, my name is Travis.");
         System.out.println("What's up man?.");
         System.out.println("Just messing with git in intellij man.");
+        System.out.println("Hey Drew, this is Shane. What's up");
     }
 }
